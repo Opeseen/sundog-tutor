dbcd9a46be2cc43e45ca87a2b2939ff4c7efdb71
Horpeyemi <Opeyemi5055@yahoo.com>
2024-08-22 18:31:32 +0100

    Upadted the source code for lambda tutor

--- v3/Site/js/config.js
+++ v3/Site/js/config.js
@@ -1,3 +1,3 @@
 var ChatApp = window.ChatApp || {};
 
-ChatApp.apiEndpoint = 'https://roxd62hsf5.execute-api.us-east-1.amazonaws.com/chatBot-prod';
+ChatApp.apiEndpoint = 'https://tdp72xtiag.execute-api.us-east-1.amazonaws.com/prod';
